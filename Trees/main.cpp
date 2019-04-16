b51a8745e31b096f303799968c0c255fef1c9122
Nitin K <infinitecontroller@gmail.com>
2019-04-16 12:54:32 -0400

    Initial Commit

---
+++ Trees/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Trees
+//
+//  Created by Nitin K on 4/16/19.
+//  Copyright © 2019 CSCI 373. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
